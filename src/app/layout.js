d54d7cb2af9ecebf18a81c61c136072db9d356bc
mayankjaat-45 <mr.singh244411@gmail.com>
2026-07-10 16:47:46 +0530

    Add Meta Pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter, Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -25,7 +26,63 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${inter.variable} ${outfit.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {
+                if(f.fbq)return;
+
+                n=f.fbq=function(){
+                  n.callMethod
+                    ? n.callMethod.apply(n,arguments)
+                    : n.queue.push(arguments);
+                };
+
+                if(!f._fbq)f._fbq=n;
+
+                n.push=n;
+                n.loaded=!0;
+                n.version='2.0';
+                n.queue=[];
+
+                t=b.createElement(e);
+                t.async=!0;
+                t.src=v;
+
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s);
+
+              }(
+                window,
+                document,
+                'script',
+                'https://connect.facebook.net/en_US/fbevents.js'
+              );
+
+              fbq('init', '1939264476755737');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1939264476755737&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
+        {children}
+      </body>
     </html>
   );
 }
